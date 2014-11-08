68567aa65b0f39865db92e94d103cbe8f7fde01d
polaris0326 <shaozhenyu0326@163.com>
2014-11-08 18:29:17 +0800

    尝试修改

--- NewBee/src/main/java/blservice_driver/accountblservice_driver/AccountBLService_BillHistory_Driver.java
+++ NewBee/src/main/java/blservice_driver/accountblservice_driver/AccountBLService_BillHistory_Driver.java
@@ -3,7 +3,14 @@ package blservice_driver.accountblservice_driver;
 import blservice.accountblservice.AccountBLService_BillHistory;
 
 public class AccountBLService_BillHistory_Driver {
+	
+	
 	public void drive(AccountBLService_BillHistory accountBLService_BillHistory){
+		
+		System.out.println("OKKKKKKK");
+		
+		
+		
 		if(accountBLService_BillHistory.billHistory(null, null, "billType", "clientName", "clerkName", "storage").add(null)){
 			System.out.println("get billhistory successfully");
 		}
